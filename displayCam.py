478027485914ff77a04df790e0349f5d89bcbc09
Audric MERLEY <audric.merley@gmail.com>
2021-01-26 10:54:17 +0100

    add view

---
+++ displayCam.py
@@ -0,0 +1,28 @@
+# This Python file uses the following encoding: utf-8
+
+from PyQt5.QtGui import QPixmap, QPen
+from PyQt5.QtWidgets import QApplication, QGraphicsScene, QGraphicsView, QGraphicsRectItem
+from PyQt5.QtCore import QRectF, Qt, QSize
+class displayCam(QGraphicsView):
+    def __init__(self):
+        QGraphicsView.__init__(self)
+        self.scene = QGraphicsScene()
+        self.setScene(self.scene)
+        self.setFixedSize(QSize(500,500))
+        self.imSize = QSize(0,0)
+    
+    def setImage(self, pixmap):
+        scaledPix = pixmap.scaled(self.size(), Qt.KeepAspectRatio, transformMode = Qt.SmoothTransformation)
+        self.scene.clear()
+        self.imSize = scaledPix.size()
+        self.scene.addPixmap(scaledPix)
+    
+    def addRectangle(self, xNormCenter, yNormCenter, widthNorm, heightNorm):
+        width = self.imSize.width()
+        height = self.imSize.height()
+        xInit = (xNormCenter - widthNorm/2.0)*width
+        yInit = (yNormCenter - heightNorm/2.0)*height
+        recItem = QGraphicsRectItem(QRectF(xInit, yInit, widthNorm*width, heightNorm*height))
+        recItem.setPen(QPen(Qt.red,3))
+        self.scene.addItem(recItem)
+
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout,QHBoxLayout,QToolButton,QLineEdit,QLabel,QFileDialog
 from PyQt5.QtGui import QPixmap
-from PyQt5.QtCore import Qt,QSize
+from PyQt5.QtCore import Qt, QSize
+from displayCam import *
 
 class Fenetre(QWidget):
     def __init__(self):
@@ -14,18 +15,20 @@ class Fenetre(QWidget):
         self.pathFinderBtn = QToolButton()
         self.launchBtn = QPushButton("Launch")
         # create label
-        self.imageLbl = QLabel("")
+        self.signName = QLabel("Choose picture")
         self.pathFinderLbl = QLabel("")
         self.pathFinderLbl.setStyleSheet("border: 1px solid black;")
-        self.imageLbl.setFixedSize(QSize(500,500))
-        self.imageLbl.setAlignment(Qt.AlignCenter)
+        # create view
+        self.view = displayCam()
+        self.view.setAlignment(Qt.AlignCenter)
         # create layout
         mainLyt = QVBoxLayout()
         pathFinderLyt = QHBoxLayout()
         mainLyt.setAlignment(Qt.AlignCenter)
         pathFinderLyt.addWidget(self.pathFinderLbl)
         pathFinderLyt.addWidget(self.pathFinderBtn)
-        mainLyt.addWidget(self.imageLbl)
+        mainLyt.addWidget(self.view)
+        mainLyt.addWidget(self.signName)
         mainLyt.addLayout(pathFinderLyt)
         mainLyt.addWidget(self.launchBtn)
         self.setLayout(mainLyt)
@@ -34,9 +37,8 @@ class Fenetre(QWidget):
         self.launchBtn.clicked.connect(self.launchProcessing)
         self.pathFinderBtn.clicked.connect(self.getImagePath)
 
-
     def launchProcessing(self):
-        self.imageLbl.setText('test')
+        print('launched')
 
     def getImagePath(self):
         dialog = QFileDialog(self, windowTitle='Selectionner image')
@@ -47,13 +49,18 @@ class Fenetre(QWidget):
         dialog.exec_()
         if len(dialog.selectedFiles()) > 0:
             im = QPixmap(dialog.selectedFiles()[0])
-            size = self.imageLbl.size()
-            scaledPix = im.scaled(size, Qt.KeepAspectRatio, transformMode = Qt.SmoothTransformation)
-            self.imageLbl.setPixmap(scaledPix)
-
+            self.view.setImage(im)
             self.pathFinderLbl.setText(dialog.selectedFiles()[0])
+            self.setBoundingBox(0.5, 0.5, 0.3, 0.3)
+            self.setSign("bounding box 0.5 0.5 0.3 0.3")
         print(dialog.selectedFiles())
 
+
+    def setBoundingBox(self, xCenterNorm, yCenterNorm, widthNorm, heightNorm):
+        self.view.addRectangle(xCenterNorm, yCenterNorm, widthNorm, heightNorm)
+
+    def setSign(self,content) :
+        self.signName.setText(content)
 
 if __name__ == '__main__':
     app = QApplication.instance()
